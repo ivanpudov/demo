6d9c7e9f614225999e5866a5ca15bf48d4822ff6
ivanpudov <i.pudov@internet.ru>
2024-08-19 11:29:14 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,42 @@
+//var str = "Have a good day!";
+//console.log(str);
+//N2
+// var age = 25;
+//console.log(age);
+//var age = undefined;
+//N3
+//var info = {{
+// name = "Ivan",
+// age = 18
+//};
+//console.log(info.name);
+//console.log(info.age);
+//var name = info.name; //console.log(name);
+//delete info.name;
+//console.log(name);
+//№4
+//var customArray = [1,2,3];
+//console.log(customArray);
+//customArray = 0;
+//console.log(customArray);
+//№5
+//var student = "vanya";
+//console.log(student);
+//student = "pudov";
+//console.log(student);
+//№6
+//var complex = [];
+//complex [0] = "my name is";
+//complex [1] = 7;
+//complex [2] = null;
+//complex [3] = info;
+//console.log(complex [2]);
+//№7
+//var complex0bj = {};
+//complex0bj.name = "ivan";
+//complex0bj.age = 18; //complex0bj.friends = ["dodik1", "dodik2", "dodik3"];
+//complex0bj.soc_links = {
+facebook: ex,
+// googlePlus: ex //); // console.log ("My name is $-{complex0bj.name}"); // console.log ("I am ${complex0bj.age}-");
+// console.log ("I have a lot of friends, but the best one is ${complex0bj.friends[0]}");
+// console.log ("You can find me on facebook: ${complex0bj.facebook} or on google groups: ${complex0bj.googlePlus}");
