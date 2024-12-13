c6c1f445ed35449f8c1438b1930b776e161573b8
IOMOPUCT <i.pudov@internet.ru>
2024-12-13 23:36:24 +0300

    Update 121.py

--- 121.py
+++ 121.py
@@ -31,3 +31,19 @@ for i in range(2550):
 # Завершаем
 t.hideturtle()  # Скрываем черепаху
 turtle.done()  # Завершаем графический цикл
+
+import multiprocessing
+import streamlit as st
+from backend import *
+
+st.title("Turtle App")
+title = st.text_input("Canvas Title", value="My Canvas")
+width = st.number_input("Canvas Width", value=500)
+height = st.number_input("Canvas Height", value=500)
+length = st.number_input("Square Length", value=200)
+clicked = st.button("Paint")
+
+t = multiprocessing.Process(target=canvas_builder, args=(title, width, height, length,))
+
+if clicked:
+   t.start()
